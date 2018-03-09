dedb8af903d70c039b46ea58f6ceb4e1a84d0716
Benedikt Rötsch <info@benedikt-roetsch.de>
2018-03-09 17:27:00 +0100

    style(code): implements eslint & prettier
    BREAKING CHANGE: lib return values are now camelCased

---
+++ .eslintrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: ['prettier-standard']
+}
--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
 // Requires run function and runs it to seperate CLI and node API's.
-const sqip = require('./index.js');
-sqip.run();
+const sqip = require('./index.js')
+
+sqip.run()
