747c57bd3bf281e33396e88248767ccca1b487aa
Benedikt Rötsch <benedikt@contentful.com>
2018-07-11 18:18:53 +0200

    repare babel for async rewrite

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,8 @@ module.exports = {
   extends: ['prettier-standard', 'plugin:node/recommended'],
   rules: {
     // As CLI tool, we want to take care of exit codes and error output on our own
-    'no-process-exit': 0
+    'no-process-exit': 0,
+    // For babel-polyfill
+    'node/no-extraneous-require': 0
   }
 }
--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 const yargs = require('yargs')
 
 const sqip = require('./index.js')
