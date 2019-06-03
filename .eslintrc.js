40e613cbd709117741a66321c33fd7db5cfa2cd4
Benedikt Rötsch <info@benedikt-roetsch.de>
2019-06-04 01:06:17 +0200

    build(monorepo): upgrade dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   parser: 'babel-eslint',
-  parserOptions: { ecmaVersion: 2018 },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
   plugins: ['node', 'jest'],
   extends: [
     'prettier-standard',
