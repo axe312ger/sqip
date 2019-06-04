53de1bf5b2ee8a275e5f9bdecac12827f9bec5de
Benedikt Rötsch <info@benedikt-roetsch.de>
2019-06-04 23:44:03 +0200

    test(sqip-plugin-data-uri): add simple snapshot test to test base64

--- packages/sqip-plugin-data-uri/__tests__/unit/sqip-plugin-data-uri.test.js
+++ packages/sqip-plugin-data-uri/__tests__/unit/sqip-plugin-data-uri.test.js
@@ -1,5 +1,9 @@
-// import sqipPluginDatauri from '../../src/sqip-plugin-datauri'
+import sqipPluginDataUri from '../../src/sqip-plugin-data-uri'
 
-describe('sqip-plugin-datauri', () => {
-  it('needs tests', () => {})
+describe('sqip-plugin-data-uri', () => {
+  it('encodes svg with base64', () => {
+    const plugin = new sqipPluginDataUri()
+    const result = plugin.apply('<svg />')
+    expect(result).toMatchSnapshot()
+  })
 })
