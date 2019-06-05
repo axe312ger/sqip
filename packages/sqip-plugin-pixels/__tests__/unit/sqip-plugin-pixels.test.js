049f0cad68ea4ea0dcbb48022da1c7ffa44ff340
Benedikt Rötsch <info@benedikt-roetsch.de>
2019-06-05 15:35:30 +0200

    test(sqip-plugin-pixels): test pixels by counting rects since sharp has slightly different output de

--- packages/sqip-plugin-pixels/__tests__/unit/sqip-plugin-pixels.test.js
+++ packages/sqip-plugin-pixels/__tests__/unit/sqip-plugin-pixels.test.js
@@ -1,5 +1,6 @@
 import sqipPluginPixels from '../../src/sqip-plugin-pixels'
 import { resolve } from 'path'
+import cheerio from 'cheerio'
 
 const FILE_DEMO_BEACH = resolve(__dirname, '../../../..', 'demo', 'beach.jpg')
 
@@ -7,6 +8,12 @@ describe('sqip-plugin-pixels', () => {
   it('turns raster image into pixelated svg', async () => {
     const plugin = new sqipPluginPixels({ input: FILE_DEMO_BEACH })
     const result = await plugin.apply()
-    expect(result).toMatchSnapshot()
+
+    const $ = cheerio.load(result, { xml: true })
+
+    // Should be one svg with 8 * 5 pixel rects
+    expect($('svg')).toHaveLength(1)
+    const $rects = $('svg > rect')
+    expect($rects).toHaveLength(8 * 5)
   })
 })
