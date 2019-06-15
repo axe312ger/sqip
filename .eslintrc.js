015fbeebd95c49997b907933311f94d1c2ee7332
Benedikt Rötsch <info@benedikt-roetsch.de>
2019-06-15 23:33:33 +0200

    style(eslint): add eslint:recommended rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,15 +6,19 @@ module.exports = {
   },
   plugins: ['node', 'jest'],
   extends: [
+    'eslint:recommended',
     'plugin:node/recommended',
     'plugin:jest/recommended'
   ],
   env: {
-    'jest/globals': true
+    'jest/globals': true,
+    es6: true,
+    node: true
   },
   rules: {
     // As CLI tool, we want to take care of exit codes and error output on our own
     'no-process-exit': 0,
+    'no-console': 0,
     // We have babel now
     'node/no-unsupported-features': 0,
     'node/no-unsupported-features/es-syntax': 0
