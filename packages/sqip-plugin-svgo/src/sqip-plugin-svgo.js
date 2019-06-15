bb471de29e5a382269277bd66d641a6bc8582c6c
Benedikt Rötsch <info@benedikt-roetsch.de>
2019-06-15 23:12:49 +0200

    fix(sqip-plugin-svgo): fix default svgo plugins for maximum optimization

--- packages/sqip-plugin-svgo/src/sqip-plugin-svgo.js
+++ packages/sqip-plugin-svgo/src/sqip-plugin-svgo.js
@@ -2,12 +2,60 @@ import SVGO from 'svgo'
 
 // SVGO with settings for maximum compression to optimize the Primitive-generated SVG
 export default class SVGOPlugin {
-  constructor(options = {}) {
+  constructor({ pluginOptions }) {
     this.options = {
       multipass: true,
       floatPrecision: 1,
-      plugins: [{ removeViewBox: false }],
-      ...options
+      plugins: [
+        { removeDoctype: true },
+        { removeXMLProcInst: true },
+        { removeComments: true },
+        { removeMetadata: true },
+        { removeXMLNS: true },
+        { removeEditorsNSData: true },
+        { cleanupAttrs: true },
+        { inlineStyles: true },
+        { minifyStyles: true },
+        { convertStyleToAttrs: true },
+        { cleanupIDs: true },
+        { prefixIds: true },
+        { removeRasterImages: true },
+        { removeUselessDefs: true },
+        { cleanupNumericValues: true },
+        { cleanupListOfValues: true },
+        { convertColors: true },
+        { removeUnknownsAndDefaults: true },
+        { removeNonInheritableGroupAttrs: true },
+        { removeUselessStrokeAndFill: true },
+        { removeViewBox: false },
+        { cleanupEnableBackground: true },
+        { removeHiddenElems: true },
+        { removeEmptyText: true },
+        { convertShapeToPath: true },
+        { moveElemsAttrsToGroup: true },
+        { moveGroupAttrsToElems: true },
+        { collapseGroups: true },
+        { convertPathData: true },
+        { convertTransform: true },
+        { removeEmptyAttrs: true },
+        { removeEmptyContainers: true },
+        { mergePaths: true },
+        { removeUnusedNS: true },
+        { sortAttrs: true },
+        { removeTitle: true },
+        { removeDesc: true },
+        { removeDimensions: true },
+        { removeAttrs: false },
+        { removeAttributesBySelector: false },
+        { removeElementsByAttr: false },
+        { addClassesToSVGElement: false },
+        { removeStyleElement: false },
+        { removeScriptElement: false },
+        { addAttributesToSVGElement: false },
+        { removeOffCanvasPaths: true },
+        { reusePaths: true }
+      ],
+      ...pluginOptions
     }
   }
   async apply(svg) {
