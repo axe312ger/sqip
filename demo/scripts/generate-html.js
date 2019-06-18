8a6fdbc2797682f5646a7c82b708a324ef7d9b35
Benedikt Rötsch <info@benedikt-roetsch.de>
2019-06-18 18:22:56 +0200

    refactor(demo): render html skeleton outside of htm and add matomo tracking

--- demo/scripts/generate-html.js
+++ demo/scripts/generate-html.js
@@ -5,6 +5,7 @@ const convertHrtime = require('convert-hrtime')
 const prettyBytes = require('pretty-bytes')
 
 const { DATASET, variants, html } = require('./config')
+const template = require('./template')
 
 function VariantResult({
   variantName,
@@ -36,7 +37,10 @@ function VariantResult({
         : html`
             <div class="preview-wrapper" style="${wrapperStyle}">
               <img
-                class="${['preview', variantName.indexOf('lqip') !== -1 && 'lqip']
+                class="${[
+                  'preview',
+                  variantName.indexOf('lqip') !== -1 && 'lqip'
+                ]
                   .filter(Boolean)
                   .join(' ')}"
                 alt="${name}"
@@ -133,199 +137,73 @@ const Row = ({ image }) => {
 
   const indexPath = resolve(__dirname, '..', 'public', 'index.html')
 
-  await writeFile(
-    indexPath,
-    html`
-      <html>
-        <head>
-          <meta charset="UTF-8" />
-          <title>SQIP demo</title>
-          <style type="text/css">
-            body {
-              font-family: sans-serif;
-              background: black;
-              padding: 2rem;
-              color: white;
-            }
-            a {
-              color: tomato;
-            }
-            a:visited {
-              color: firebrick;
-            }
-            img.preview {
-              display: block;
-              width: 100%;
-            }
-            img.preview.lqip {
-              filter: blur(12px);
-              transform: scale(1.15);
-            }
-            table {
-              border-collapse: collapse;
-              border-spacing: 0;
-              table-layout: fixed;
-              width: 100%;
-            }
-            td {
-              position: relative;
-              padding: 0;
-            }
-            td:hover .overlay {
-              display: flex;
-            }
-            td,
-            th {
-              width: 300px;
-            }
-            th {
-              text-align: left;
-              padding: 1rem;
-            }
-            .description {
-              padding: 1rem;
-            }
-            details {
-              margin-top: 1rem;
-            }
-            .sizes {
-              position: absolute;
-              right: 0.5rem;
-              bottom: 0.5rem;
-            }
-            .overlay {
-              display: none;
-              position: absolute;
-              left: 0;
-              right: 0;
-              top: 0;
-              bottom: 0;
-              background: rgba(0, 0, 0, 0.6);
-              padding: 1rem;
-              color: white;
-              flex-direction: column;
-              justify-content: center;
-            }
-            .overlay table {
-              font-size: 0.85em;
-              table-layout: auto;
-            }
-            .overlay td,
-            .overlay th {
-              text-align: left;
-              color: white;
-              width: auto;
-              padding: 0.25rem;
-              vertical-align: top;
-            }
-            .preview-wrapper {
-              position: relative;
-              height: 0;
-              overflow: hidden;
-            }
-            .preview-wrapper img {
-              position: absolute;
-              width: 100%;
-              height: 100%;
-            }
-            p.processing-time img {
-              vertical-align: text-bottom;
-            }
-          </style>
-        </head>
-        <body>
-          <h1>
-            SQIP Demo${' '}
-            <a href="https://github.com/axe312ger/sqip">
-              <img src="./assets/github.svg" width="32" />
-            </a>
-          </h1>
-          <p>
-            SQIP will generate SVG based previews of images. They can be used as
-            a lazy-loading image preview, a video thumbnail or an artistic
-            element for your project.
-          </p>
-          <p>
-            Compare the new SQIP version with the old SQIP version, LQIP and a
-            300px thumbnail
-          </p>
-          <p>
-            <a href="https://github.com/axe312ger/sqip">
-              Learn more about SQIP
-            </a>
-          </p>
-          <table>
-            <thead>
-              <tr>
-                ${variants.map(({ title }) => {
-                  return html`
-                    <th>${title}</th>
-                  `
-                })}
-              </tr>
-              <tr>
-                ${variants.map(({ description, config }, i) => {
-                  const compressedSize = compressedSizes[i]
-                  const originalSize = compressedSizes[0]
-                  const averageBytes = Math.round(
-                    compressedSize / images.length
-                  )
-                  const originalPercent = (
-                    (compressedSize / originalSize) *
-                    100
-                  ).toFixed(2)
-                  return html`
-                    <td>
-                      <div class="description">
-                        ${description}
-                        <p class="processing-time">
-                          <img
-                            alt="average processing time"
-                            src="./assets/processing-time.svg"
-                            width="20"
-                          />
-                          <span title="Average processing time"
-                            >${(processingTimes[i] / images.length).toFixed(
-                              3
-                            )}s</span
-                          >
-                          <span title="Average size"
-                            >${' | ø '}${prettyBytes(averageBytes)}</span
-                          >
-                          <span title="Average size compared to thumbnail"
-                            >${' | ↓ '}${originalPercent}%</span
-                          >
-                        </p>
-                        ${config &&
-                          html`
-                            <details>
-                              <summary>config:</summary>
-                              <pre><code>${JSON.stringify(
-                                config,
-                                null,
-                                2
-                              )}</code></pre>
-                            </details>
-                          `}
-                      </div>
-                    </td>
-                  `
-                })}
-              </tr>
-            </thead>
-            <tbody>
-              ${images.map(
-                image =>
-                  html`
-                    <${Row} image="${image}" />
-                  `
-              )}
-            </tbody>
-          </table>
-        </body>
-      </html>
-    `
-  )
+  const pageContent = template(html`
+    <table>
+      <thead>
+        <tr>
+          ${variants.map(({ title }) => {
+            return html`
+              <th>${title}</th>
+            `
+          })}
+        </tr>
+        <tr>
+          ${variants.map(({ description, config }, i) => {
+            const compressedSize = compressedSizes[i]
+            const originalSize = compressedSizes[0]
+            const averageBytes = Math.round(compressedSize / images.length)
+            const originalPercent = (
+              (compressedSize / originalSize) *
+              100
+            ).toFixed(2)
+            return html`
+              <td>
+                <div class="description">
+                  ${description}
+                  <p class="processing-time">
+                    <img
+                      alt="average processing time"
+                      src="./assets/processing-time.svg"
+                      width="20"
+                    />
+                    <span title="Average processing time"
+                      >${(processingTimes[i] / images.length).toFixed(3)}s</span
+                    >
+                    <span title="Average size"
+                      >${' | ø '}${prettyBytes(averageBytes)}</span
+                    >
+                    <span title="Average size compared to thumbnail"
+                      >${' | ↓ '}${originalPercent}%</span
+                    >
+                  </p>
+                  ${config &&
+                    html`
+                      <details>
+                        <summary>config:</summary>
+                        <pre><code>${JSON.stringify(
+                          config,
+                          null,
+                          2
+                        )}</code></pre>
+                      </details>
+                    `}
+                </div>
+              </td>
+            `
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        ${images.map(
+          image =>
+            html`
+              <${Row} image="${image}" />
+            `
+        )}
+      </tbody>
+    </table>
+  `)
 
+  await writeFile(indexPath, pageContent)
   console.log('Done:', indexPath)
 })()
