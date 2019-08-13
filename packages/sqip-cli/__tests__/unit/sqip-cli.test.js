4b7fba944ee9cef54e117c97cfdd4dccd2a150a5
Benedikt Rötsch <opensource@axe312.dev>
2019-08-13 19:10:46 +0200

    test(sqip-cli): add snapshot test to ensure arguments are passed as expected

--- packages/sqip-cli/__tests__/unit/sqip-cli.test.js
+++ packages/sqip-cli/__tests__/unit/sqip-cli.test.js
@@ -3,15 +3,16 @@ import sqipCLI from '../../src/sqip-cli'
 
 import semver from 'semver'
 
-jest.mock('sqip', () => ({
-  default: jest.fn(),
-  resolvePlugins: jest.fn(plugins =>
+jest.mock('sqip', () => {
+  const sqipMock = jest.fn()
+  sqipMock.resolvePlugins = jest.fn(plugins =>
     plugins.map(plugin => ({
       name: plugin,
       Plugin: { cliOptions: [] }
     }))
   )
-}))
+  return sqipMock
+})
 
 const logSpy = jest.spyOn(global.console, 'log').mockImplementation(() => {})
 const errorSpy = jest
@@ -36,7 +37,7 @@ describe('sqip-plugin-cli', () => {
   it('no args: show help and require input option', async () => {
     await sqipCLI()
 
-    expect(sqip.default).not.toHaveBeenCalled()
+    expect(sqip).not.toHaveBeenCalled()
     expect(sqip.resolvePlugins.mock.calls).toMatchSnapshot('resolve')
     expect(logSpy.mock.calls).toMatchSnapshot('log')
     expect(errorSpy.mock.calls).toMatchSnapshot('error')
@@ -48,7 +49,7 @@ describe('sqip-plugin-cli', () => {
 
     await sqipCLI()
 
-    expect(sqip.default).not.toHaveBeenCalled()
+    expect(sqip).not.toHaveBeenCalled()
     expect(sqip.resolvePlugins.mock.calls).toMatchSnapshot('resolve')
     expect(semver.valid(logSpy.mock.calls[0][0])).toBe(logSpy.mock.calls[0][0])
     expect(errorSpy.mock.calls).toMatchSnapshot('error')
@@ -60,10 +61,30 @@ describe('sqip-plugin-cli', () => {
 
     await sqipCLI()
 
-    expect(sqip.default).not.toHaveBeenCalled()
+    expect(sqip).not.toHaveBeenCalled()
     expect(sqip.resolvePlugins.mock.calls).toMatchSnapshot('resolve')
     expect(logSpy.mock.calls).toMatchSnapshot('log')
     expect(errorSpy.mock.calls).toMatchSnapshot('error')
     expect(global.process.exit).toHaveBeenCalledWith(0)
+  })
+
+  it('passes all given args to library', async () => {
+    process.argv = [
+      null,
+      null,
+      '-i',
+      'foo',
+      '--output',
+      'bar',
+      '-w',
+      '1337',
+      '-m',
+      '4'
+    ]
+
+    await sqipCLI()
+
+    expect(sqip).toHaveBeenCalled()
+    expect(sqip.mock.calls).toMatchSnapshot()
   })
 })
