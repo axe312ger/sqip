0f370ded3377bdb32114ebb16c4a686e41ad4efd
axe312ger <opensource@axe312.dev>
2021-06-21 14:58:18 +0200

    adjust eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,19 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint'
-    // 'jest'
-  ],
+  plugins: ['@typescript-eslint', 'jest'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
-    'prettier/@typescript-eslint'
-    // 'plugin:node/recommended',
-    // 'plugin:jest/recommended'
+    'plugin:jest/recommended'
   ],
 
-  // parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module'
   },
-  // plugins: ['node', 'jest'],
+
   env: {
     'jest/globals': true,
     es6: true,
@@ -28,9 +22,6 @@ module.exports = {
   rules: {
     // As CLI tool, we want to take care of exit codes and error output on our own
     'no-process-exit': 0,
-    'no-console': 0,
-    // We have babel now
-    'node/no-unsupported-features': 0,
-    'node/no-unsupported-features/es-syntax': 0
+    'no-console': 0
   }
 }
